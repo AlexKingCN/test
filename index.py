bacf5eeb3e267b2d2ab5884dabf7708cbebea32f
alex king <wlx_king@163.com>
2018-04-17 22:43:46 +0800

    添加代码
    添加代码

---
+++ index.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @des     : 练习代码
+# @Time    : 2018/3/29 21:52
+# @Author  : Alex King
+# @Site    : 
+# @File    : index.py
+
+from math import log
+import operator
+import matplotlib.pyplot as plt
+import pickle
+
+# 创建数据集
+def createDataSet():
+    # dataSet = [[1, 1, 'yes'],
+    #            [1, 1, 'yes'],
+    #            [1, 0, 'no'],
+    #            [0, 1, 'no'],
+    #            [0, 1, 'no']]
+    # labels = ['no surfacing','flippers']
+    dataSet = [['youth', 'no', 'no', '1', 'refuse'],
+               ['youth', 'no', 'no', '2', 'refuse'],
+               ['youth', 'yes', 'no', '2', 'agree'],
+               ['youth', 'yes', 'yes', '1', 'agree'],
+               ['youth', 'no', 'no', '1', 'refuse'],
+               ['mid', 'no', 'no', '1', 'refuse'],
+               ['mid', 'no', 'no', '2', 'refuse'],
+               ['mid', 'yes', 'yes', '2', 'agree'],
+               ['mid', 'no', 'yes', '3', 'agree'],
+               ['mid', 'no', 'yes', '3', 'agree'],
+               ['elder', 'no', 'yes', '3', 'agree'],
+               ['elder', 'no', 'yes', '2', 'agree'],
+               ['elder', 'yes', 'no', '2', 'agree'],
+               ['elder', 'yes', 'no', '3', 'agree'],
+               ['elder', 'no', 'no', '1', 'refuse'],
+               ]
+    labels = ['age', 'working?', 'house?', 'credit_situation']
+    return dataSet, labels
+
+
+# 计算数据集的乡农熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        # 统计每个结果出现的频数
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob,2)
+    return shannonEnt
+
+# 按照给定特征划分数据集
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            # 剔除所选的特征
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+# 选择最好的数据集划分方式
+def chooseBestFeatureToSplit(dataSet):
+    # 特征个数
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    # 遍历每个特征
+    for i in range(numFeatures):
+        # 获取该特征的所有取值
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        # 遍历每个取值，求得
+        for value in uniqueVals:
+            # 获得子数据集
+            subDataSet = splitDataSet(dataSet, i, value)
+            # 计算子数据集的乡农熵
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        # 计算信息增益（信息增益越大，分类能力越大）
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+# 使用完特征值后还没有归为一类，投票最多数的为该类最终结果
+def majorityCnt(classList):
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+
+# 创建决策树
+def createTree(dataSet,labels):
+    # 备份lables，防止对labels产生影响
+    reLables = labels[:]
+    classList = [example[-1] for example in dataSet]
+    # 结束条件
+    # 1、划归为一类
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 2、用完所有特征值
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    # 存储每次递归分类最佳属性
+    # 下标
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = reLables[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    # 删除已分类特征
+    del(reLables[bestFeat])
+    # 开始划分
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = reLables[:]       #copy all of labels, so trees don't mess up existing labels
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
+    return myTree
+
+
+# 绘制决策树（可视化）
+# 定义文本框和箭头格式
+decisionNode = dict(boxstyle="round4", color='#3366FF')  # 定义判断结点形态
+leafNode = dict(boxstyle="circle", color='#FF6633')  # 定义叶结点形态
+arrow_args = dict(arrowstyle="<-", color='g')  # 定义箭头
+#计算叶结点数
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':# 测试结点的数据类型是否为字典
+            numLeafs += getNumLeafs(secondDict[key])
+        else:   numLeafs +=1
+    return numLeafs
+# 计算树的深度
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':# 测试结点的数据类型是否为字典
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:   thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+# 绘制带箭头的注释
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt,  xycoords='axes fraction',
+             xytext=centerPt, textcoords='axes fraction',
+             va="center", ha="center", bbox=nodeType, arrowprops=arrow_args )
+# 在父子结点间填充文本信息
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)  # 计算宽与高
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)  # 标记子结点属性值
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD # 减少y偏移
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],cntrPt,str(key))        #recursion
+        else:   #it's a leaf node print the leaf node
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+#if you do get a dictonary you know it's a tree, and the first element will be another dict
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;
+    plotTree(inTree, (0.5,1.0), '')
+    plt.show()
+
+
+# 使用决策树进行决策（检测）
+def classify(inputTree,featLabels,testVec):
+    '''
+           利用决策树进行分类
+    :param: inputTree:构造好的决策树模型
+    :param: featLabels:所有的类标签
+    :param: testVec:测试数据
+    :return: 分类决策结果
+    '''
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    key = testVec[featIndex]
+    valueOfFeat = secondDict[key]
+    if isinstance(valueOfFeat, dict):
+        classLabel = classify(valueOfFeat, featLabels, testVec)
+    else:
+        classLabel = valueOfFeat
+    return classLabel
+
+
+# 使用pickle存储决策树
+def storeTree(myTree):
+    fw = open('myTree.txt','w')
+    pickle.dump(myTree,fw)
+    fw.closed
+
+# 读取决策树
+def loadTree(filename):
+    fr = open(filename)
+    return pickle.load(fr)
+
+
+if __name__ == "__main__":
+    dataSet,labels = createDataSet()
+    # shangEnt = calcShannonEnt(dataSet)
+    # retDataSet = splitDataSet(dataSet,1,1)
+    # shangEntRet = calcShannonEnt(retDataSet)
+    myTree =  createTree(dataSet, labels)
+    # dataSet,labels 已经改变
+    # print myTree
+    # createPlot(myTree)
+    storeTree(myTree)
+    # myTree = loadTree('myTree')
+
+    result = classify(myTree,labels,['elder', 'no', 'yes', '2'])
+    print result
